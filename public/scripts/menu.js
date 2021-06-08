890079a04eb79be1f9ef16d5bcbd962d1a911658
Oleg Mamiev <olegios12@gmail.com>
2021-06-08 22:11:03 +0300

    feat: menu script

---
+++ public/scripts/menu.js
@@ -0,0 +1,13 @@
+(function () {
+    let i = 0;
+    const txt = 'Уровень откроется 20-го июня в 19:00';
+    const speed = 100;
+
+    (function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById('timer-wrapper').innerHTML += txt[i];
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    })();
+})();
